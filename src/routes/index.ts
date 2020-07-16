b83ef932409b6412ee54aa3ba189c12210882b02
Luiz Barros <luizbarros.incode@gmail.com>
2020-07-16 16:49:56 -0300

    Adiciona funcionalidade de debug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) =>
+routes.get('/users', (request, response) =>
   response.json({ message: 'Start from here!' })
 );
 
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import routes from './routes/index';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 app.listen(3333, () => console.log('\nUp and Running ! ! !'));
